303effc9da513e68e2a571808f7cdbe8a8a9f19e
224lpalm@frhsd.com <224lpalm@frhsd.com>
2024-04-25 08:45:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var yourName = prompt("Enter your name: ");
+var occupation = prompt("Enter your favorite place to go to: ");
+var salary = prompt("Enter what you did at this place (past tense verb): ");
+var yourEXP = prompt("Enter when you went to this place: ");
+var verb = prompt("Who did you go with? ");
+var pluralNoun = prompt("what was the weather? (adjective) ");
+
+document.write(yourName + " just got off , and for the first day of spring break went to the " + occupation + ", and " + salary + " all day. " + yourEXP + " is when you went and had so much fun, " + " enjoying time with " + verb + " on this " + pluralNoun + " day.");
